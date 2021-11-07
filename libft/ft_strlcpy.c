3fb23ebe930b846acbf8175bff43fb9eb80a63e4
Patapoulpinette <91835278+Patapoulpinette@users.noreply.github.com>
2021-11-07 21:18:25 +0100

    minor updates : strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,15 +6,15 @@ size_t  ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsiz
 
     i = 0;
     if (dstsize == 0)
-        return (0);
+        return (ft_strlen(src));
     else
     {
-        while (i != '\0' && i < dstsize - 1)
+        while (i < ft_strlen(src) && i < dstsize - 1)
         {
             dst[i] = src[i];
             i++;
         }
     }
     dst[i] = '\0';
-    return (i);
+    return (ft_strlen(src));
 }
