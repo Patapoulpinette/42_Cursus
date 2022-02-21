05255810b934b9f809f6f8833249b0510b6ddd5e
Delphine Bouron <dbouron@z4r9p1.42lyon.fr>
2022-02-21 18:09:53 +0100

    GNL is working, still have to split it into functions

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 15:03:23 by dbouron           #+#    #+#             */
-/*   Updated: 2022/02/21 17:52:22 by dbouron          ###   ########lyon.fr   */
+/*   Updated: 2022/02/21 18:04:23 by dbouron          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,58 +24,46 @@ char	*get_next_line(int fd)
 
 	if (fd == -1 || BUFFER_SIZE < 1 || read(fd, 0, 0) == -1)
 		return (NULL);
-	//puts("Les parametres donnes sont ok");
 	while (ft_mystrchr(backup, '\n') == -1 && ft_mystrchr(buffer, '\n') == -1)
 	{
-		//puts("Je rentre dans la boucle while car il n'y a pas de \\n");
 		size = read(fd, buffer, BUFFER_SIZE);
 		if (size == -1)
 			return (NULL);
 		if (size == 0)
 		{
+			if (!backup)
+				return (NULL);
+			result = ft_strjoin(backup, "\0");
 			free(backup);
 			backup = NULL;
-			return (NULL);
+			return (result);
 		}
 		buffer[size] = '\0';
-		//printf("Apres avoir lu :\n - size = %d\n - buffer = %s\n", size, buffer);
 		if (ft_mystrchr(buffer, '\n') != -1)
 		{
-			//puts("Je rentre dans le if car il y a un \\n dans le buffer");
 			result = ft_strjoin(backup, ft_substr(buffer, 0, ft_mystrchr(buffer, '\n')));
 			if (!result)
 				return (NULL);
-			//printf("result = %s\n", result);
-			//printf("start = %d, len = %lu\n", ft_mystrchr(buffer, '\n') + 1, ft_strlen(buffer));
 			if (ft_mystrchr(buffer, '\n') + 1 == (int)ft_strlen(buffer))
-			{
-				//puts("Je suis dans le if");
 				backup = NULL;
-			}
 			else
 			{
-				//puts("Je suis dans le else");
 				backup = ft_substr(buffer, ft_mystrchr(buffer, '\n') + 1, ft_strlen(buffer) - ft_mystrchr(buffer, '\n') - 1);
 				if (!backup)
 					return (NULL);
-				//printf("Il y a un \\n dans buffer donc la fin de buffer va dans backup = %s\n", backup);
 			}
 			return (result);
 		}
 		backup = ft_strjoin(backup, buffer);
-		//printf("Pas de \\n dans buffer donc tout va dans :\n - backup = %s\n", backup);
 	}
 	if (ft_mystrchr(backup, '\n') != -1)
 	{
-		//puts("Est-ce que je rentre dans ce if ?");
 		result = ft_substr(backup, 0, ft_mystrchr(backup, '\n'));
 		if (!result)
 			return (NULL);
-		//puts("lecture de backup et result ok");
 		backup = ft_substr(backup, ft_mystrchr(backup, '\n') + 1, ft_strlen(backup) - ft_mystrchr(backup, '\n') - 1);
 		if (!backup)
 			return (NULL);
-		//printf("Il y a un \\n dans backup donc la fin de backup va dans backup = %s\n", backup);
 		return (result);
 	}
 	return (NULL);
