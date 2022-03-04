c050d16471af348eac67ed2f9ecd26154550c4a8
Delphine Bouron <dbouron@z2r3p2.42lyon.fr>
2022-03-04 18:19:38 +0100

    correction name of file in header

--- get_next_line_2/get_next_line_bonus.h
+++ get_next_line_2/get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:08:30 by dbouron           #+#    #+#             */
-/*   Updated: 2022/02/24 15:08:33 by dbouron          ###   ########lyon.fr   */
+/*   Updated: 2022/03/04 18:16:51 by dbouron          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
