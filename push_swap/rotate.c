26053bbe092587073aaef59f3d8ab33850d07bc9
Delphine Bouron <dbouron@z2r4p1.42lyon.fr>
2022-04-11 12:20:19 +0200

    rotate tested

--- push_swap/rotate.c
+++ push_swap/rotate.c
@@ -6,9 +6,11 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 10:23:56 by dbouron           #+#    #+#             */
-/*   Updated: 2022/04/07 10:36:08 by dbouron          ###   ########.fr       */
+/*   Updated: 2022/04/11 12:19:07 by dbouron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	rotate_a(char *str)
 {
@@ -24,7 +26,7 @@ void	rotate_a(char *str)
 		str[i] = str[i + 1];
 		i++;
 	}
-	str[i] = temp;
+	str[i - 1] = temp;
 }
 
 void	rotate_b(char *str)
@@ -41,11 +43,21 @@ void	rotate_b(char *str)
 		str[i] = str[i + 1];
 		i++;
 	}
-	str[i] = temp;
+	str[i - 1] = temp;
 }
 
 void	rotate_ab(char *str_a, char *str_b)
 {
 	rotate_a(str_a);
 	rotate_b(str_b);
+}
+
+int	main(void)
+{
+	char	a[10] = "123456789";
+	char	b[4] = "987";
+
+	dprintf(2, "a : %s\nb : %s\n", a, b);
+	rotate_ab(a, b);
+	dprintf(2, "a : %s\nb : %s\n", a, b);
 }
