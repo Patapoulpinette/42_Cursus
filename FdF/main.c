a8e822eed77d2a90dc29d2c81420a15fd2e21a57
Delphine Bouron <dbouron@student.42lyon.fr>
2022-05-23 19:25:26 +0200

    TODO list updated

--- FdF/main.c
+++ FdF/main.c
@@ -6,16 +6,13 @@
 /*   By: dbouron <dbouron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 15:33:54 by dbouron           #+#    #+#             */
-/*   Updated: 2022/05/23 11:15:01 by dbouron          ###   ########.fr       */
+/*   Updated: 2022/05/23 19:16:52 by dbouron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-//TODO - parsing : GNL [✓] + split [✓] + atoi
-//à chaque ligne lue (GNL) y++ et x revient à 0
-//indice  tableau = x
-//Puis, pour chaque point p je dessine une ligne avec le point suivant et le point du dessous (en gros i + 1 et j + 1)
+//Pour chaque point je dessine une ligne avec le point suivant et le point du dessous (en gros i + 1 et j + 1)
 
 int	main(int argc, char **argv)
 {
