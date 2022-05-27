cd1a72f6c2a764ec13748f7eb8d6ac171c9a6ccf
Delphine Bouron <dbouron@student.42lyon.fr>
2022-05-27 16:06:52 +0200

    Bresenham algo OK, trying to display in isometric projection

--- FdF/algorithm.c
+++ FdF/algorithm.c
@@ -3,61 +3,108 @@
 /*                                                        :::      ::::::::   */
 /*   algorithm.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbouron <dbouron@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:02:13 by dbouron           #+#    #+#             */
-/*   Updated: 2022/05/21 15:06:18 by dbouron          ###   ########.fr       */
+/*   Updated: 2022/05/27 15:57:57 by dbouron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void drawline(t_image *image, int x0, int y0, int x1, int y1, int color)
+void	iso(int *x, int *y, int z)
 {
-	t_algo_params	algo_params;
+	int	previous_x;
+	int	previous_y;
 
-	algo_params.x = x0;
-	algo_params.y = y0;
-	algo_params.dx = x1 - x0;
-	algo_params.dy = y1 - y0;
-	if (x1 >= y1)
+	previous_x = *x;
+	previous_y = *y;
+	*x = (previous_x - previous_y) * cos(0.523599);
+	*y = -z + (previous_x + previous_y) * sin(0.523599);
+}
+
+void	bhm_line(t_image *image, t_maps_coord *maps_coord, int x1, int y1, int x2, int y2, int color)
+{
+	t_algo_params	algo;
+
+	algo.dx = x2 - x1;
+	algo.dy = y2 - y1;
+	algo.dx1 = abs(algo.dx);
+	algo.dy1 = abs(algo.dy);
+	algo.px = 2 * algo.dy1 - algo.dx1;
+	algo.py = 2 * algo.dx1 - algo.dy1;
+	if (algo.dy1 <= algo.dx1)
 	{
-		algo_params.p = 2 * algo_params.dy - algo_params.dx;
-		while (algo_params.x < x1)
+		if (algo.dx >= 0)
 		{
-			if (algo_params.p >= 0)
+			algo.x = x1;
+			algo.y = y1;
+			algo.xe = x2;
+		}
+		else
+		{
+			algo.x = x2;
+			algo.y = y2;
+			algo.xe = x1;
+		}
+		my_mlx_pixel_put(image, maps_coord, algo.x, algo.y, color);
+		while (algo.x < algo.xe)
+		{
+			algo.x = algo.x + 1;
+			if (algo.px < 0)
 			{
-				//dprintf(2, "if | x = %d | y = %d\n", algo_params.x, algo_params.y);
-				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
-				algo_params.y = algo_params.y + 1;
-				algo_params.p = algo_params.p + 2 * algo_params.dy - 2 * algo_params.dx;
-			} else
-			{
-				//dprintf(2, "else | x = %d | y = %d\n", algo_params.x, algo_params.y);
-				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
-				algo_params.p = algo_params.p + 2 * algo_params.dy;
+				algo.px = algo.px + 2 * algo.dy1;
 			}
-			algo_params.x = algo_params.x + 1;
+			else
+			{
+				if ((algo.dx < 0 && algo.dy < 0) || (algo.dx > 0 && algo.dy > 0))
+				{
+					algo.y = algo.y + 1;
+				}
+				else
+				{
+					algo.y = algo.y - 1;
+				}
+				algo.px = algo.px + 2 * (algo.dy1 - algo.dx1);
+			}
+			my_mlx_pixel_put(image, maps_coord, algo.x, algo.y, color);
 		}
 	}
-	if (x1 < y1)
+	else
 	{
-		algo_params.p = 2 * algo_params.dx - algo_params.dy;
-		while (algo_params.y < y1)
+		if (algo.dy >= 0)
 		{
-			if (algo_params.p >= 0)
+			algo.x = x1;
+			algo.y = y1;
+			algo.ye = y2;
+		}
+		else
+		{
+			algo.x = x2;
+			algo.y = y2;
+			algo.ye = y1;
+		}
+		my_mlx_pixel_put(image, maps_coord, algo.x, algo.y, color);
+		while (algo.y < algo.ye)
+		{
+			algo.y = algo.y + 1;
+			if (algo.py <= 0)
 			{
-				//dprintf(2, "if | x = %d | y = %d\n", algo_params.x, algo_params.y);
-				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
-				algo_params.x = algo_params.x + 1;
-				algo_params.p = algo_params.p + 2 * algo_params.dx - 2 * algo_params.dy;
-			} else
-			{
-				//dprintf(2, "else | x = %d | y = %d\n", algo_params.x, algo_params.y);
-				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
-				algo_params.p = algo_params.p + 2 * algo_params.dx;
+				algo.py = algo.py + 2 * algo.dx1;
 			}
-			algo_params.y = algo_params.y + 1;
+			else
+			{
+				if ((algo.dx < 0 && algo.dy < 0) || (algo.dx > 0 && algo.dy > 0))
+				{
+					algo.x = algo.x + 1;
+				}
+				else
+				{
+					algo.x = algo.x - 1;
+				}
+				algo.py = algo.py + 2 * (algo.dx1 - algo.dy1);
+			}
+			my_mlx_pixel_put(image, maps_coord, algo.x, algo.y, color);
 		}
 	}
 }
---
+++ FdF/algorithm_ex.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 14:02:13 by dbouron           #+#    #+#             */
+/*   Updated: 2022/05/27 14:13:18 by dbouron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+typedef struct s_algo_params
+{
+	int		x;
+	int		y;
+	int		dx;
+	int		dy;
+	float	m;
+	int		p;
+}				t_algo_params;
+
+void	drawline(t_image *image, int x0, int y0, int x1, int y1, int color)
+{
+	t_algo_params	algo_params;
+
+	algo_params.x = x0;
+	algo_params.y = y0;
+	algo_params.dx = x1 - x0;
+	algo_params.dy = y1 - y0;
+	if (x1 >= y1)
+	{
+		algo_params.p = 2 * algo_params.dy - algo_params.dx;
+		while (algo_params.x < x1)
+		{
+			if (algo_params.p >= 0)
+			{
+				//dprintf(2, "if | x = %d | y = %d\n", algo_params.x, algo_params.y);
+				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
+				algo_params.y = algo_params.y + 1;
+				algo_params.p = algo_params.p + 2 * algo_params.dy - 2 * algo_params.dx;
+			}
+			else
+			{
+				//dprintf(2, "else | x = %d | y = %d\n", algo_params.x, algo_params.y);
+				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
+				algo_params.p = algo_params.p + 2 * algo_params.dy;
+			}
+			algo_params.x = algo_params.x + 1;
+		}
+	}
+	if (x1 < y1)
+	{
+		algo_params.p = 2 * algo_params.dx - algo_params.dy;
+		while (algo_params.y < y1)
+		{
+			if (algo_params.p >= 0)
+			{
+				//dprintf(2, "if | x = %d | y = %d\n", algo_params.x, algo_params.y);
+				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
+				algo_params.x = algo_params.x + 1;
+				algo_params.p = algo_params.p + 2 * algo_params.dx - 2 * algo_params.dy;
+			}
+			else
+			{
+				//dprintf(2, "else | x = %d | y = %d\n", algo_params.x, algo_params.y);
+				my_mlx_pixel_put(image, algo_params.x, algo_params.y, color);
+				algo_params.p = algo_params.p + 2 * algo_params.dx;
+			}
+			algo_params.y = algo_params.y + 1;
+		}
+	}
+}
