4d576cfadc15a352bcf6c71804b0aed25ade26cf
Delphine Bouron <dbouron@student.42lyon.fr>
2022-07-07 15:58:03 +0200

    parsing2 in progress

--- Philosophers/parsing2.c
+++ Philosophers/parsing2.c
@@ -6,13 +6,25 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:48:48 by dbouron           #+#    #+#             */
-/*   Updated: 2022/07/07 15:51:45 by dbouron          ###   ########.fr       */
+/*   Updated: 2022/07/07 15:57:17 by dbouron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	parsing(int argc, char **arg)
+void	parsing2(int argc, char **argv)
 {
-	//try to check every verif on each arg
+	int	j;
+
+	j = 1;
+	if (argc == 5 || argc == 6)
+	{
+		while (argv[j])
+		{
+			//try to check every verif on each arg
+			j++;
+		}
+	}
+	else
+		print_parsing_error(-1);
 }
